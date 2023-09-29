9608a6871d1e85f73f050d4699f5aad225efe619
Anir Zaher <azaher@c4r8p5.1337.ma>
2023-09-29 15:32:14 +0100

    3alam khor

---
+++ includes/cub3D.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaher <azaher@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 16:51:42 by azaher            #+#    #+#             */
+/*   Updated: 2023/09/28 19:08:51 by azaher           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include "../libraries/ft_printf/ft_printf.h"
+#include "../libraries/libft/libft.h"
+#include "../libraries/queue/queue.h"
+#include "../libraries/get_next_line/get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_map
+{
+    char    **map;
+    int     W_map;
+    int     H_map;
+    int     S_map;
+}   t_map;
+
+typedef struct s_player
+{
+    float   xpos;
+    float   ypos;
+    float   velocity;
+    float   rotation_speed;
+    float   player_angle;
+}   t_player;
+
+typedef struct s_data
+{
+    char    *N_texture;
+    char    *E_texture;
+    char    *W_texture;
+    char    *S_texture;
+    int     f;
+    int     c;
+}  t_data;
+
+typedef struct s_game
+{
+    t_data  data;
+    int     player_x;
+    int     player_y;
+    int     map_h;
+    int     map_w;
+    char    **map;
+}   t_game;
+
+void print_error(char *error);
+
+
+#endif
