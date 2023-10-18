2509debacf913d1e188555a9d03d90145d5e4535
Anir Zaher <azaher@e2r3p1.1337.ma>
2023-10-18 18:17:48 +0100

    fixed some norm stuff

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,64 +6,63 @@
 /*   By: azaher <azaher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:51:42 by azaher            #+#    #+#             */
-/*   Updated: 2023/10/16 14:26:39 by azaher           ###   ########.fr       */
+/*   Updated: 2023/10/18 18:17:18 by azaher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define	CUB3D_H
+# define CUB3D_H
 
-#include "../libraries/ft_printf/ft_printf.h"
-#include "../libraries/libft/libft.h"
-#include "../libraries/queue/queue.h"
-#include "../libraries/get_next_line/get_next_line.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+# include "../libraries/ft_printf/ft_printf.h"
+# include "../libraries/libft/libft.h"
+# include "../libraries/queue/queue.h"
+# include "../libraries/get_next_line/get_next_line.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 typedef struct s_map
 {
 	char	**map;
-	int		W_map;
-	int		H_map;
-	int		S_map;
-}   t_map;
+	int		w_map;
+	int		h_map;
+	int		s_map;
+}	t_map;
 
 typedef struct s_player
 {
-	float   xpos;
-	float   ypos;
-	float   velocity;
-	float   rotation_speed;
-	float   player_angle;
-}   t_player;
+	float	xpos;
+	float	ypos;
+	float	velocity;
+	float	rotation_speed;
+	float	player_angle;
+}	t_player;
 
 typedef struct s_data
 {
-	char	*N_texture;
-	char	*E_texture;
-	char	*W_texture;
-	char	*S_texture;
-	int 	f;
-	int 	c;
-}  t_data;
+	char	*n_texture;
+	char	*e_texture;
+	char	*w_texture;
+	char	*s_texture;
+	int		f;
+	int		c;
+}	t_data;
 
 typedef struct s_game
 {
-	t_data  data;
-	int     player_x;
-	int     player_y;
-	int     map_h;
-	int     map_w;
-	char    **map;
-}   t_game;
+	t_data	data;
+	int		player_x;
+	int		player_y;
+	int		map_h;
+	int		map_w;
+	char	**map;
+}	t_game;
 
-void print_error(char *error);
-void init_game(t_game *game);
-int ft_strcmp(const char *s1, const char *s2);
+void	print_error(char *error);
+void	init_game(t_game *game);
+int		ft_strcmp(const char *s1, const char *s2);
 void	free_2d(char **arr);
-int	arrlen(char **arr);
-
+int		arrlen(char **arr);
 
 #endif
